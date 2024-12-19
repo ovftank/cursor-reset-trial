add4515ded5066707a7f2f543e197d726e399cf8
Tuấn Anh <ovftank@gmail.com>
2024-12-19 22:15:38 +0700

    chore: init commit

---
+++ build.py
@@ -0,0 +1,23 @@
+import subprocess
+
+
+def create_executable():
+    icon = "images/icon.ico"
+    pyinstaller_command = [
+        'pyinstaller',
+        '--noconfirm',
+        '--onefile',
+        '--windowed',
+        '--uac-admin',
+        '--icon', icon,
+        '--add-data', f'{icon};.',
+        'main.py'
+    ]
+    result = subprocess.run(pyinstaller_command,
+                            capture_output=True, text=True)
+    print(result.stdout)
+    print(result.stderr)
+
+
+if __name__ == '__main__':
+    create_executable()
